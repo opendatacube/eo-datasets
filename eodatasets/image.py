67eebaf06f28e7287c0bff6661553f442bf48462
Jeremy Hooke <jez@stulk.com>
2015-03-27 19:19:14 +1100

    Slight cleanup of ULA3 thumbnail code.

--- eodatasets/image.py
+++ eodatasets/image.py
@@ -1,120 +1,149 @@
 import glob
-import os, logging, subprocess, math, errno, re
-import  gdal, gdalconst
-import numpy.ma as ma
+import logging
+import os
+from subprocess import check_call
+import math
+import errno
+import tempfile
+
+import gdalconst
+import gdal
+import numpy
+import time
 
 
-logger = logging.getLogger('root.' + __name__)
+_LOG = logging.getLogger(__name__)
+
+
+def run_command(command, work_dir):
+    _LOG.debug('Running %r', command)
+    check_call(command, cwd=work_dir)
+    _LOG.debug('Finished %s', command[0])
 
 
 def create_thumbnail(red_file, green_file, blue_file, thumbnail_image,
                      outcols=1024, nodata=-999, work_dir=None, overwrite=True):
-    """Create JPEG thumbnail image using individual R, G, B images.
-    Arguments:
-        red_file: red band data file
-        green_file: green band data file
-        blue_file: blue band data file
-        thumbnail_image: Name of thumbnail image file
-        outcols: thumbnail image width
-        nodata: null/fill data value
+    """
+        Create JPEG thumbnail image using individual R, G, B images.
+
+    :param red_file: red band data file
+    :param green_file: green band data file
+    :param blue_file: blue band data file
+    :param thumbnail_image: thumbnail file to write to.
+    :param outcols: thumbnail width
+    :param nodata: null/fill data value
+    :param work_dir: temp/work directory to use.
+    :param overwrite: overwrite existing thumbnail?
+
     Thumbnail height is adjusted automatically to match the aspect ratio
     of the input images.
+
     """
 
-    # working files
-    file_to = "RGB.vrt"
-    warp_to_file = "RGBwarped.vrt"
-    outtif = "thumbnail.tif"
+    if os.path.exists(thumbnail_image) and not overwrite:
+        _LOG.warning('File already exists. Skipping creation of %s', thumbnail_image)
+        return
 
-    if work_dir:
-        file_to = os.path.join(work_dir, file_to)
-        warp_to_file = os.path.join(work_dir, warp_to_file)
-        outtif = os.path.join(work_dir, outtif)
+    # thumbnail_image = os.path.abspath(thumbnail_image)
+
+    work_dir = os.path.abspath(work_dir) if work_dir else tempfile.mkdtemp('gaip-package')
+
+    # working files
+    file_to = os.path.join(work_dir, 'rgb.vrt')
+    warp_to_file = os.path.join(work_dir, 'rgb-warped.vrt')
+    outtif = os.path.join(work_dir, 'thumbnail.tif')
+
+    # file_to = os.path.abspath(file_to)
 
     # Build the RGB Virtual Raster at full resolution
-    subprocess.call(["gdalbuildvrt", "-overwrite", "-separate", file_to, red_file, green_file, blue_file], cwd=work_dir)
+    run_command(["gdalbuildvrt", "-overwrite", "-separate", file_to, red_file, green_file, blue_file], work_dir)
+    assert os.path.exists(file_to), "VRT must exist"
 
     # Determine the pixel scaling to get the correct width thumbnail
     vrt = gdal.Open(file_to)
     intransform = vrt.GetGeoTransform()
     inpixelx = intransform[1]
-    #inpixely = intransform[5]
+    # inpixely = intransform[5]
     inrows = vrt.RasterYSize
     incols = vrt.RasterXSize
-    #print inrows,incols
-    outresx = inpixelx*incols/outcols
-    outrows = int(math.ceil((float(inrows)/float(incols))*outcols))
-    #print outresx, outcols, outrows
 
-    if (overwrite or not os.path.exists(thumbnail_image)):
-        subprocess.call(["gdalwarp", "-of", "VRT", "-tr", str(outresx), str(outresx), "-r", "near", "-overwrite", file_to, warp_to_file], cwd=work_dir)
+    outresx = inpixelx * incols / outcols
+    outrows = int(math.ceil((float(inrows) / float(incols)) * outcols))
 
-        # Open VRT file to array
-        vrt = gdal.Open(warp_to_file)
-        bands = (1,2,3)
-        driver = gdal.GetDriverByName ("GTiff")
-        outdataset = driver.Create(outtif,outcols,outrows, 3, gdalconst.GDT_Byte)
-        #rgb_composite = numpy.zeros((outrows,outcols,3))
+    run_command([
+        "gdalwarp", "-of", "VRT",
+        "-tr", str(outresx), str(outresx),
+        "-r", "near",
+        "-overwrite", file_to,
+        warp_to_file
+    ], work_dir)
 
-        # Loop through bands and apply Scale and Offset
-        for bandnum, band in enumerate(bands):
-            vrtband = vrt.GetRasterBand(band)
-            vrtband_array = vrtband.ReadAsArray()
-            nbits=gdal.GetDataTypeSize(vrtband.DataType)
-            #print nbits
-            dfScaleDstMin,dfScaleDstMax=0.0,255.0
+    # Open VRT file to array
+    vrt = gdal.Open(warp_to_file)
+    bands = (1, 2, 3)
+    driver = gdal.GetDriverByName("GTiff")
+    outdataset = driver.Create(outtif, outcols, outrows, 3, gdalconst.GDT_Byte)
 
-            # Determine scale limits
-            #dfScaleSrcMin = dfBandMean - 2.58*(dfBandStdDev)
-            #dfScaleSrcMax = dfBandMean + 2.58*(dfBandStdDev)
+    # Loop through bands and apply Scale and Offset
+    for bandnum, band in enumerate(bands):
 
-            if (nbits == 16):
-                count = 32767 + int(nodata)
-                histogram = vrtband.GetHistogram(-32767, 32767, 65536)
-            else:
-                count = 0
-                histogram = vrtband.GetHistogram()
-            total = 0
+        start = time.time()
+        vrtband = vrt.GetRasterBand(band)
+        vrtband_array = vrtband.ReadAsArray()
+        nbits = gdal.GetDataTypeSize(vrtband.DataType)
+        dfScaleDstMin, dfScaleDstMax = 0.0, 255.0
 
-            cliplower = int(0.01*(sum(histogram)-histogram[count]))
-            clipupper = int(0.99*(sum(histogram)-histogram[count]))
-            #print sum(histogram)
-            #print cliplower,clipupper
-            #print histogram[31768]
-            while total < cliplower and count < len(histogram)-1:
-                count = count+1
-                total = int(histogram[count])+total
-                dfScaleSrcMin = count
-            #print "total",total
-            if (nbits == 16):
-                count = 32767 + int(nodata)
-            else: count = 0
-            #print "count for max",count
-            total = 0
-            while total < clipupper and count < len(histogram)-1:
-                count = count+1
-                #print count,clipupper,total
-                total = int(histogram[count])+total
-                dfScaleSrcMax = count
+        if nbits == 16:
+            count = 32767 + int(nodata)
+            histogram = vrtband.GetHistogram(-32767, 32767, 65536)
+        else:
+            count = 0
+            histogram = vrtband.GetHistogram()
+        total = 0
 
-            if (nbits == 16):
-                dfScaleSrcMin = dfScaleSrcMin - 32768
-                dfScaleSrcMax = dfScaleSrcMax - 32768
+        cliplower = int(0.01 * (sum(histogram) - histogram[count]))
+        clipupper = int(0.99 * (sum(histogram) - histogram[count]))
 
-            # Determine gain and offset
-            dfScale = (dfScaleDstMax - dfScaleDstMin) / (dfScaleSrcMax - dfScaleSrcMin)
-            dfOffset = -1 * dfScaleSrcMin * dfScale + dfScaleDstMin
+        while total < cliplower and count < len(histogram) - 1:
+            count += 1
+            total += int(histogram[count])
+            dfScaleSrcMin = count
 
-            # Apply gain and offset
-            outdataset.GetRasterBand(band).WriteArray((ma.masked_less_equal(vrtband_array, int(nodata))*dfScale)+dfOffset)
+        if nbits == 16:
+            count = 32767 + int(nodata)
+        else:
+            count = 0
 
-        outdataset = None
+        total = 0
+        while total < clipupper and count < len(histogram) - 1:
+            count += 1
+            total += int(histogram[count])
+            dfScaleSrcMax = count
 
-        # GDAL Create doesn't support JPEG so we need to make a copy of the GeoTIFF
-        subprocess.call(["gdal_translate", "-of", "JPEG", outtif, thumbnail_image])
+        if nbits == 16:
+            dfScaleSrcMin -= 32768
+            dfScaleSrcMax -= 32768
 
-    else:
-        logger.warning('File already exists. Skipping creation of %s', thumbnail_image)
+        # Determine gain and offset
+        dfScale = (dfScaleDstMax - dfScaleDstMin) / (dfScaleSrcMax - dfScaleSrcMin)
+        dfOffset = -1 * dfScaleSrcMin * dfScale + dfScaleDstMin
+
+        _LOG.info('Scaling param calculated in %s', time.time()-start)
+        start = time.time()
+
+        # Apply gain and offset
+        outdataset.GetRasterBand(band).WriteArray(
+            (numpy.ma.masked_less_equal(vrtband_array, int(nodata)) * dfScale) + dfOffset
+        )
+
+        _LOG.info('Band in %s', time.time()-start)
+
+    # Must close dataset to flush to disk.
+    # noinspection PyUnusedLocal
+    outdataset = None
+
+    # GDAL Create doesn't support JPEG so we need to make a copy of the GeoTIFF
+    run_command(["gdal_translate", "-of", "JPEG", outtif, thumbnail_image], work_dir)
 
     # Clean up work files
     for f in [file_to, warp_to_file, outtif]:
@@ -124,12 +153,12 @@ def create_thumbnail(red_file, green_file, blue_file, thumbnail_image,
             if e.errno != errno.ENOENT:
                 raise
 
-    return (outcols, outrows)
+    return outcols, outrows
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    logger.setLevel(logging.INFO)
+    _LOG.setLevel(logging.INFO)
     #     red_band=7,
     # green_band=5,
     # blue_band=1
