757f138a0bc3d84e57047ead746c3b4f31b6abd7
Jeremy Hooke <jez@stulk.com>
2015-04-15 17:16:46 +1000

    Remove outdated test runners.

--- eodatasets/metadata/tests/mtl/__init__.py
+++ eodatasets/metadata/tests/mtl/__init__.py
@@ -1,7 +1,5 @@
-import unittest
+from pathlib import Path
 
-import datetime
-from pathlib import Path, PosixPath
 from eodatasets.type import *
 from eodatasets.metadata import mtl
 import eodatasets.type as ptype
@@ -25,40 +23,6 @@ def assert_expected_mtl(mtl_file, expected_ds, base_folder=Path('/tmp/fake-folde
     _assert_same(ds, expected_ds)
 
 
-class TestMtlRead(unittest.TestCase):
-    def test_ls8_equivalence(self):
-
-        mtl_path = Path(os.path.join(os.path.dirname(__file__), 'data', 'ls8_mtl.txt'))
-
-        ds = DatasetMetadata(id_=uuid.UUID('3ff71eb0-d5c5-11e4-aebb-1040f381a756'))
-        ds = mtl.populate_from_mtl(ds, mtl_path, base_folder=Path('/tmp/fake-folder'))
-
-        _assert_same(ds, _EXPECTED_LS8_OUT)
-
-        # Sanity check: different dataset_id is not equal.
-        ds = DatasetMetadata()
-        ds = mtl.populate_from_mtl(ds, mtl_path, base_folder=Path('/tmp/fake-folder'))
-        self.assertNotEqual(ds, _EXPECTED_LS8_OUT)
-
-    def test_ls5_equivalence(self):
-
-        mtl_path = Path(os.path.join(os.path.dirname(__file__), 'data', 'ls5_definitive_mtl.txt'))
-
-        ds = DatasetMetadata(id_=uuid.UUID('3ff71eb0-d5c5-11e4-aebb-1040f381a756'))
-        ds = mtl.populate_from_mtl(ds, mtl_path, base_folder=Path('/tmp/fake-folder'))
-
-        _assert_same(ds, _EXPECTED_LT5_OUT)
-
-    def test_ls7_equivalence(self):
-
-        mtl_path = Path(os.path.join(os.path.dirname(__file__), 'data', 'ls7_predictive_mtl.txt'))
-
-        ds = DatasetMetadata(id_=uuid.UUID('3ff71eb0-d5c5-11e4-aebb-1040f381a756'))
-        ds = mtl.populate_from_mtl(ds, mtl_path, base_folder=Path('/tmp/fake-folder'))
-
-        _assert_same(ds, _EXPECTED_L7_OUT)
-
-
 def _assert_same(o1, o2, prefix=''):
     """
     Assert the two are equal.
@@ -73,7 +37,7 @@ def _assert_same(o1, o2, prefix=''):
     """
 
     def _compare(k, val1, val2):
-        _assert_same(val1, val2, prefix=prefix+'.'+str(k))
+        _assert_same(val1, val2, prefix=prefix + '.' + str(k))
 
     if isinstance(o1, SimpleObject):
         assert o1.__class__ == o2.__class__, "Differing classes %r: %r and %r" \
@@ -97,15 +61,7 @@ def _assert_same(o1, o2, prefix=''):
         raise AssertionError("Mismatch for property %r:  %r != %r" % (prefix, o1, o2))
 
 
-def load_tests(loader=None, standard_tests=None, pattern=None):
-    # top level directory cached on loader instance
-    this_dir = os.path.dirname(__file__)
-    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
-    standard_tests.add
-    return standard_tests
-
-
 if __name__ == '__main__':
-
     import unittest
+
     unittest.main()
