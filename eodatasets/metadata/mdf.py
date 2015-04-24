fe76867ea9b54371508aca83dc2bca80998fc7bc
Jeremy Hooke <jez@stulk.com>
2015-04-24 16:58:01 +1000

    Extract usgs ids from common sibling MDF filenames.

--- eodatasets/metadata/mdf.py
+++ eodatasets/metadata/mdf.py
@@ -15,6 +15,25 @@ LS8_SENSORS = {"C": "OLI_TIRS", "O": "OLI", "T": "TIRS"}
 _log = logging.getLogger(__name__)
 
 
+def _before_underscore(s):
+    """
+    :type s: str
+    :rtype: str
+
+    >>> _before_underscore('LC80880750762013254ASA00_IDF.xml')
+    'LC80880750762013254ASA00'
+    >>> _before_underscore('LC80880750762013254ASA00_MD5.txt')
+    'LC80880750762013254ASA00'
+    >>> _before_underscore('383.000.2013137232105971.ASA')
+    >>> _before_underscore('_MD5.txt')
+    ''
+    """
+    if '_' not in s:
+        return None
+
+    return s.split('_')[0]
+
+
 def extract_md(base_md, directory_path):
     """
     Extract metadata from a directory of MDF files
@@ -44,25 +63,31 @@ def extract_md(base_md, directory_path):
 
     directory_path, files = find_mdf_files(directory_path)
 
-    if not directory_path or len(files) < 1:
-        _log.debug("No MDF data found")
+    if len(files) < 1:
+        _log.debug("No MDF files found")
+        return base_md
+
+    usgs_id = directory_path.name if directory_path else None
+
+    if not usgs_id:
+        # Look at siblings of the mdf files.
+        for f in list(files)[0].parent.iterdir():
+            prefix = _before_underscore(f.name)
+            if is_mdf_usgs_id(prefix):
+                _log.info('Found usgs id %r', usgs_id)
+                usgs_id = prefix
+
+    if not usgs_id:
+        _log.debug('No MDF id matched. Assuming not MDF.')
         return base_md
 
     _log.info("Found MDF files %r in directory %r", files, directory_path)
 
-    return _extract_mdf_file_data(base_md, directory_path, files)
+    if usgs_id:
+        base_md = _extract_mdf_id_fields(base_md, usgs_id)
 
-
-def _extract_mdf_file_data(base_md, directory, files):
-    """
-
-    :type base_md: ptype.DatasetMetadata
-    :type directory: pathlib.Path
-    :type files: list of pathlib.Path
-    :rtype: ptype.DatasetMetadata
-    """
-    base_md = _extract_mdf_directory_fields(base_md, directory)
-    base_md = _extract_mdf_file_fields(base_md, files)
+    if files:
+        base_md = _extract_mdf_file_fields(base_md, [f.name for f in files])
 
     base_md.product_type = 'RAW'
     base_md.ga_level = 'P00'
@@ -74,11 +99,11 @@ def _extract_mdf_file_data(base_md, directory, files):
     return base_md
 
 
-def _extract_mdf_directory_fields(base_md, directory):
+def _extract_mdf_id_fields(base_md, mdf_usgs_id):
     """
     V I N ppp RRR rrr YYYY ddd GSI vv
     :type base_md: ptype.DatasetMetadata
-    :type directory: pathlib.Path
+    :type mdf_usgs_id: str
     :rtype: ptype.DatasetMetadata
     """
     m = re.search(
@@ -91,11 +116,11 @@ def _extract_mdf_directory_fields(base_md, directory):
         "(?P<acq_date>\d{7})" +
         "(?P<gsi>\w{3})" +
         "(?P<version>\d{2})",
-        directory.name)
+        mdf_usgs_id)
 
     fields = m.groupdict()
 
-    base_md.usgs_dataset_id = directory.name
+    base_md.usgs_dataset_id = mdf_usgs_id
 
     if not base_md.platform:
         base_md.platform = ptype.PlatformMetadata()
@@ -129,7 +154,7 @@ def _extract_mdf_directory_fields(base_md, directory):
     return base_md
 
 
-def _extract_mdf_file_fields(base_md, files):
+def _extract_mdf_file_fields(base_md, mdf_file_names):
     """
     From http://landsat.usgs.gov/documents/LDCM-DFCB-001.pdf...
 
@@ -149,13 +174,13 @@ def _extract_mdf_file_fields(base_md, files):
       for example 383.000.2013137232105971.ASA
 
     :type base_md: ptype.DatasetMetadata
-    :type files: list of pathlib.Path
+    :type mdf_file_names: list of str
     :rtype: ptype.DatasetMetadata
     """
 
     times = []
 
-    for f in files:
+    for f_name in mdf_file_names:
         m = re.search(
             "(?P<root_file_number>\d{3})" +
             "\." +
@@ -164,7 +189,7 @@ def _extract_mdf_file_fields(base_md, files):
             "(?P<date_time>\d{16})" +
             "\." +
             "(?P<gsi>\w{3})",
-            f.name)
+            f_name)
 
         fields = m.groupdict()
 
@@ -176,6 +201,8 @@ def _extract_mdf_file_fields(base_md, files):
     start = min(times) - datetime.timedelta(seconds=60)  # 60 seconds before first segment's acquisition complete time
     stop = max(times)  # the last segment's acquisition complete time
 
+    if not base_md.acquisition:
+        base_md.acquisition = ptype.AcquisitionMetadata()
     base_md.acquisition.aos = base_md.acquisition.aos or start
     base_md.acquisition.los = base_md.acquisition.los or stop
 
@@ -194,33 +221,36 @@ def adjust_time(t, delta):
     return time.gmtime(calendar.timegm(t) + delta)
 
 
-def is_mdf_directory(directory):
+def is_mdf_usgs_id(name):
     """
     Is this an MDF directory name?
-    :type directory: str or unicode
+    :type directory: str or unicode or None
     :rtype: bool
 
-    >>> is_mdf_directory('LC80920740862013090LGN00')
+    >>> is_mdf_usgs_id('LC80920740862013090LGN00')
     True
-    >>> is_mdf_directory('LC81070620632013228ASA00')
+    >>> is_mdf_usgs_id('LC81070620632013228ASA00')
     True
-    >>> is_mdf_directory('NPP.VIIRS.7686.ALICE')
+    >>> is_mdf_usgs_id('NPP.VIIRS.7686.ALICE')
     False
-    >>> is_mdf_directory('TERRA.72239.S1A2C2D4R4')
+    >>> is_mdf_usgs_id('TERRA.72239.S1A2C2D4R4')
     False
-    >>> is_mdf_directory('TERRA.72239.S1A2C2D4R4')
+    >>> is_mdf_usgs_id('TERRA.72239.S1A2C2D4R4')
     False
-    >>> is_mdf_directory('LANDSAT-8.1725')
+    >>> is_mdf_usgs_id('LANDSAT-8.1725')
     False
-    >>> is_mdf_directory('LC80910760902013148ASA00')
+    >>> is_mdf_usgs_id('LC80910760902013148ASA00')
     True
-    >>> is_mdf_directory('133.004.2013148000120310.ASA')
+    >>> is_mdf_usgs_id('133.004.2013148000120310.ASA')
     False
-    >>> is_mdf_directory('LC80910760902013148ASA00_IDF.xml')
+    >>> is_mdf_usgs_id('LC80910760902013148ASA00_IDF.xml')
     False
-
+    >>> is_mdf_usgs_id(None)
+    False
     """
-    return bool(re.match("^L[OTC]\d{17}[A-Z]{3}\d{2}$", directory))
+    if not name:
+        return False
+    return bool(re.match("^L[OTC]\d{17}[A-Z]{3}\d{2}$", name))
 
 
 def is_mdf_file(filename):
@@ -257,12 +287,12 @@ def find_mdf_files(directory):
         return {f for f in mdf_dir.iterdir() if is_mdf_file(f.name)}
 
     # Were we given the MDF directory itself?
-    if is_mdf_directory(directory.name):
+    if is_mdf_usgs_id(directory.name):
         mdf_dir = directory
         return mdf_dir, _get_mdf_files(mdf_dir)
 
     # Is there a single MDF sub-directory?
-    mdf_subdirs = [d for d in directory.iterdir() if is_mdf_directory(d.name)]
+    mdf_subdirs = [d for d in directory.iterdir() if is_mdf_usgs_id(d.name)]
     if mdf_subdirs and len(mdf_subdirs) == 1:
         return mdf_subdirs[0], _get_mdf_files(mdf_subdirs[0])
 
@@ -283,10 +313,10 @@ def find_mdf_files(directory):
     mdf_files = _get_mdf_files(directory)
 
     if mdf_files:
-        if is_mdf_directory(directory.parent.name):
+        if is_mdf_usgs_id(directory.parent.name):
             mdf_dir = directory.parent
 
-        if is_mdf_directory(directory.parent.parent.name):
+        if is_mdf_usgs_id(directory.parent.parent.name):
             mdf_dir = directory.parent.parent
 
     return mdf_dir, mdf_files
--- eodatasets/metadata/tests/test_mdf.py
+++ eodatasets/metadata/tests/test_mdf.py
@@ -1,17 +1,17 @@
 import unittest
 import datetime
+import uuid
 
 import pathlib
-
 import eodatasets.metadata.mdf as mdf
-from eodatasets.tests import write_files
+from eodatasets.tests import write_files, assert_same
 import eodatasets.type as ptype
 
 
 class MdfTests(unittest.TestCase):
     def test_directory_parse(self):
         dataset_id = 'LC80880750762013254ASA00'
-        metadata = mdf._extract_mdf_directory_fields(ptype.DatasetMetadata(), pathlib.Path(dataset_id))
+        metadata = mdf._extract_mdf_id_fields(ptype.DatasetMetadata(), dataset_id)
 
         self.assertEquals(metadata.usgs_dataset_id, dataset_id)
         self.assertEquals(metadata.platform.code, 'LANDSAT_8')
@@ -52,6 +52,45 @@ class MdfTests(unittest.TestCase):
 
         # It should also work when given the specific MDF folder.
         metadata = mdf.extract_md(ptype.DatasetMetadata(), d.joinpath('LC80880750762013254ASA00'))
+        _test_mdf_output(metadata)
+
+    def test_unchanged_without_id(self):
+        # No MDF directory, only files. Don't try to extract information from the files.
+        d = write_files({
+            '446.000.2013254233714881.ASA': 'a',
+            '447.000.2013254233711482.ASA': 'a',
+        })
+
+        id_ = uuid.uuid1()
+        date = datetime.datetime.utcnow()
+        metadata = mdf.extract_md(ptype.DatasetMetadata(id_=id_, creation_dt=date), d)
+        # Should be unchanged: No USGS ID found.
+        assert_same(metadata, ptype.DatasetMetadata(id_=id_, creation_dt=date))
+
+    def test_files_with_usgs_id(self):
+        # No MDF directory, only files. Can we still extract enough info?
+        d = write_files({
+            '446.000.2013254233714881.ASA': 'a',
+            '447.000.2013254233711482.ASA': 'a',
+            'LC80880750762013254ASA00_IDF.xml': 'a',
+            'LC80880750762013254ASA00_MD5.txt': 'a',
+            })
+
+        def _test_mdf_output(metadata):
+            self.assertEquals(metadata.usgs_dataset_id, 'LC80880750762013254ASA00')
+            self.assertEquals(metadata.platform.code, 'LANDSAT_8')
+            self.assertEquals(metadata.instrument.name, 'OLI_TIRS')
+            self.assertEqual(metadata.format_.name, 'MDF')
+            self.assertEqual(metadata.ga_level, 'P00')
+            self.assertEquals(metadata.image.satellite_ref_point_start, ptype.Point(88, 75))
+            self.assertEquals(metadata.image.satellite_ref_point_end, ptype.Point(88, 76))
+            self.assertEquals(metadata.acquisition.groundstation.code, 'ASA')
+            # No longer bother with vague center date.
+            # self.assertEqual(metadata.extent.center_dt, datetime.date(2013, 9, 11))
+            self.assertEqual(metadata.acquisition.aos, datetime.datetime(2013, 9, 11, 23, 36, 11, 482000))
+            self.assertEqual(metadata.acquisition.los, datetime.datetime(2013, 9, 11, 23, 37, 14, 881000))
+
+        metadata = mdf.extract_md(ptype.DatasetMetadata(), d)
         _test_mdf_output(metadata)
 
     def test_find_mdf_directory(self):
@@ -121,12 +160,12 @@ class MdfTests(unittest.TestCase):
         self.assertEqual((None, set()), mdf.find_mdf_files(d))
 
         # Make sure that metadata is not modified when no MDF is found.
-        startind_md = ptype.DatasetMetadata()
-        id_ = startind_md.id_
-        creation_dt = startind_md.creation_dt
+        starting_md = ptype.DatasetMetadata()
+        id_ = starting_md.id_
+        creation_dt = starting_md.creation_dt
         expected_dt = ptype.DatasetMetadata(id_=id_, creation_dt=creation_dt)
 
-        output = mdf.extract_md(startind_md, d)
+        output = mdf.extract_md(starting_md, d)
         self.assertEqual(expected_dt, output)
 
 
