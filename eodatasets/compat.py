b89cf432619fdec1c36a0a8067b599172fb97c8f
Jeremy Hooke <jez@stulk.com>
2015-04-26 16:55:16 +1000

    Add compat.py explanation.

--- eodatasets/compat.py
+++ eodatasets/compat.py
@@ -1,4 +1,10 @@
 # coding=utf-8
+"""
+Compatibility helpers for Python 2 and 3.
+
+See: http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/
+
+"""
 import sys
 
 PY2 = sys.version_info[0] == 2
