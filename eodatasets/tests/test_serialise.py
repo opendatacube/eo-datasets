bc17fe8aa4cd2146973dde9b021d5de294a834c4
Jeremy Hooke <jez@stulk.com>
2015-05-05 12:11:40 +1000

    Fix flatten hypothesis test.

--- eodatasets/tests/test_serialise.py
+++ eodatasets/tests/test_serialise.py
@@ -6,8 +6,13 @@ from __future__ import absolute_import
 
 from eodatasets.tests import write_files, TestCase, slow
 from eodatasets import serialise, compat, type as ptype
-from hypothesis import given
-from hypothesis.specifiers import dictionary
+from hypothesis import given, strategy
+from hypothesis.specifiers import dictionary, strings
+
+strings_without_trailing_underscore = strategy(
+    strings([chr(i) for i in range(128)])
+) \
+    .filter(lambda s: not s.endswith('_'))
 
 
 class TestSerialise(TestCase):
@@ -54,11 +59,24 @@ class TestSerialise(TestCase):
         )
 
     @slow
-    @given(dictionary(str, int))
+    @given(dictionary(strings_without_trailing_underscore, int))
     def test_flat_dict_flattens_identically(self, dict_):
+        print(dict_)
         self.assert_items_equal(
-            dict_,
+            dict_.items(),
             serialise.as_flat_key_value(dict_)
+        )
+
+    @slow
+    @given(dictionary(str, int))
+    def test_flat_dict_flattens_without_underscore_suffix(self, dict_):
+        # A (single) trailing underscore should be stripped from key names if present, as these are added
+        # for python name conflicts.
+
+        # If we append an underscore to every key, the result should be identical.
+        self.assert_items_equal(
+            dict_.items(),
+            serialise.as_flat_key_value({k + '_': v for k, v in dict_.items()})
         )
 
     def test_key_value_simple_obj(self):
