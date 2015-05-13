4a22ed9e8a1a064294d6a5fe1ee34923ecfc3a12
Jeremy Hooke <jez@stulk.com>
2015-05-14 09:34:54 +1000

    Improve test dict comparison error message.

--- tests/__init__.py
+++ tests/__init__.py
@@ -55,12 +55,11 @@ def assert_same(o1, o2, prefix=''):
         for i, val in enumerate(o1):
             _compare(i, val, o2[i])
     elif isinstance(o1, dict) and isinstance(o2, dict):
-        assert len(o1) == len(o2), "Differing lengths: %s\n\t%r\n\t%r" % (prefix, o1, o2)
-
         for k, val in o1.items():
             assert k in o2, "%s[%r] is missing.\n\t%r\n\t%r" % (prefix, k, o1, o2)
-            _compare(k, val, o2[k])
-
+        for k, val in o2.items():
+            assert k in o1, "%s[%r] is missing.\n\t%r\n\t%r" % (prefix, k, o2, o1)
+            _compare(k, val, o1[k])
     elif o1 != o2:
         sys.stderr.write("%r\n" % o1)
         sys.stderr.write("%r\n" % o2)
