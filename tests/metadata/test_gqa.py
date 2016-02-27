89754b0689912dbaa1041d65858abd31613e0f5b
Jeremy Hooke <jez@stulk.com>
2016-02-27 11:43:12 +1100

    Update gqa tests for yaml input.

--- tests/metadata/test_gqa.py
+++ tests/metadata/test_gqa.py
@@ -12,7 +12,7 @@ from eodatasets.type import DatasetMetadata
 
 _LOG = logging.getLogger(__name__)
 
-_GQA_PATH = Path(__file__).absolute().parent.joinpath('20141201_20010425_B6_gqa_results.yaml')
+_GQA_PATH = Path(__file__).absolute().parent.joinpath('20130818_20000119_B5_gqa_results.yaml')
 
 
 def test_gqa():
@@ -20,28 +20,24 @@ def test_gqa():
     gqa.populate_from_gqa(md, _GQA_PATH)
     print(repr(md.gqa))
     assert md.gqa == {
-        'abs_iterative_mean_residual_x': 1.3,
-        'abs_iterative_mean_residual_y': 1.2,
-        'acq_day': datetime.date(2014, 12, 1),
-        # Bands are always strings. They can have odd names ("6_vcid_1").
-        'band': '6',
-        'blue': 120,
-        'cep90': 212.0,
-        'final_gcp_count': 1493,
-        'green': 340,
-        'iterative_mean_residual_x': -0.4,
-        'iterative_mean_residual_y': 0.5,
-        'iterative_stddev_residual_x': 2.5,
-        'iterative_stddev_residual_y': 2.5,
-        'mean_residual_x': -0.4,
-        'mean_residual_y': 0.5,
-        'red': 321,
-        'ref_day': datetime.date(2001, 4, 25),
-        'residual_x': 1.9,
-        'residual_y': 1.8,
-        'sceneid': 'LS8_OLITIRS_OTH_P51_GALPGS01-032_099_072_20141201',
-        'stddev_residual_x': 3.6,
-        'stddev_residual_y': 3.6,
-        'teal': 735,
-        'yellow': 98,
+        'stddev_residual': {'y': 4.524645392606803, 'x': 4.23604905517344},
+        'abs_iterative_mean_residual': {'y': 4.081039145021645, 'x': 8.281189264069264},
+        'acq_day': datetime.date(2013, 8, 18),
+        'residual': {'y': 4.070382, 'x': 8.266335},
+        'colors': {
+            'teal': 0.0,
+            'blue': 10.0,
+            'yellow': 25.0,
+            'green': 0.0,
+            'red': 274.0
+        },
+        'ref_day': datetime.date(2000, 1, 19),
+        'ref_source': 'GLS_v1',
+        'iterative_mean_residual': {'y': 0.9739366558441558, 'x': 8.281189264069264},
+        'mean_residual': {'y': 0.9739366558441558, 'x': 8.281189264069264},
+        'iterative_stddev_residual': {'y': 4.524645392606803, 'x': 4.23604905517344},
+        'cep90': 440.00546272953613,
+        # Bands are always strings. They can have odd names ("5_vcid_1")
+        'band': '5',
+        'final_gcp_count': 308
     }
