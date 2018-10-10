fc38923746819a7cc0b90644555fc69f43f53746
Jeremy Hooke <jez@stulk.com>
2018-10-10 13:41:39 +1100

    Combine s2-prepare commands into one

--- eodatasets/scripts/genprepare.py
+++ eodatasets/scripts/genprepare.py
@@ -1,9 +1,6 @@
 import click
 
-from ..prepare.s2_prepare_cophub_zip import (
-    from_list as s2_scihub_from_list,
-    from_args as s2_scihub_from_args
-)
+from ..prepare.s2_prepare_cophub_zip import main as s2_scihub
 from ..prepare.ls_usgs_l1_prepare import main as ls_usgs
 from ..prepare.s2_l1c_aws_pds_prepare import main as s2_awspds
 
@@ -14,7 +11,10 @@ def run():
     pass
 
 
-run.add_command(s2_scihub_from_args, name='s2-cophub')
-run.add_command(s2_scihub_from_list, name='s2-cophub-list')
+run.add_command(s2_scihub, name='s2-cophub')
 run.add_command(ls_usgs, name='ls-usgs')
 run.add_command(s2_awspds, name='s2-awspds')
+
+
+if __name__ == '__main__':
+    run()
