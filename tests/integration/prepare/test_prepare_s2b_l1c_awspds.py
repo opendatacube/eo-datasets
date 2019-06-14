e73473c7e09ffca05d53693b13382fcb2f77856f
Jeremy Hooke <jez@stulk.com>
2019-06-14 15:28:59 +1000

    s2 tests don't expect tuple/list normalisation

--- tests/integration/prepare/test_prepare_s2b_l1c_awspds.py
+++ tests/integration/prepare/test_prepare_s2b_l1c_awspds.py
@@ -243,4 +243,5 @@ def test_prepare_s2a_l1c_safe(tmpdir):
         ],
         expected_doc=expected_doc,
         expected_metadata_path=expected_metadata_path,
+        normalise_tuples=False,
     )
