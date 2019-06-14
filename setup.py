f473b86b4a9b69fe000e5191bf68d9f8e1d249b7
Jeremy Hooke <jez@stulk.com>
2019-06-14 14:22:22 +1000

    Add pyproj dependency.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ import versioneer
 
 
 tests_require = [
-    'deepdiff',
-    'flake8',
-    'hypothesis',
-    'mock',
-    'pep8-naming',
-    'pytest',
-    'pytest-flake8',
-    'python-rapidjson',
+    "deepdiff",
+    "flake8",
+    "hypothesis",
+    "mock",
+    "pep8-naming",
+    "pytest",
+    "pytest-flake8",
+    "python-rapidjson",
 ]
 
 
@@ -23,35 +23,32 @@ setup(
     description="Packaging, metadata and provenance for GA EO datasets",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(exclude=('tests', 'tests.*')),
-    package_data={
-        '': ['*.json'],
-    },
+    packages=find_packages(exclude=("tests", "tests.*")),
+    package_data={"": ["*.json"]},
     install_requires=[
-        'attrs',
-        'boltons',
-        'cattrs',
-        'click',
-        'python-dateutil',
-        'checksumdir',
-        'ciso8601',
-        'gdal',
-        'jsonschema',
-        'numpy',
-        'PyYAML!=5.1',
-        'netCDF4',
-        'rasterio',
-        'ruamel.yaml',
-        'shapely',
-        'scipy',
-        'structlog',
-        'h5py',
+        "attrs",
+        "boltons",
+        "cattrs",
+        "click",
+        "python-dateutil",
+        "checksumdir",
+        "ciso8601",
+        "gdal",
+        "jsonschema",
+        "numpy",
+        "PyYAML!=5.1",
+        "pyproj",
+        "netCDF4",
+        "rasterio",
+        "ruamel.yaml",
+        "shapely",
+        "scipy",
+        "structlog",
+        "h5py",
     ],
     tests_require=tests_require,
-    extras_require={
-        'test': tests_require
-    },
-    entry_points='''
+    extras_require={"test": tests_require},
+    entry_points="""
         [console_scripts]
         eod-package=eodatasets.scripts.genpackage:run
         eod-validate=eodatasets.prepare.validate:run
@@ -59,5 +56,5 @@ setup(
         eod-generate-browse=eodatasets.scripts.genbrowse:run
         eod-prepare=eodatasets.scripts.genprepare:run
         eod-recompress-tar=eodatasets.scripts.recompress:main
-    ''',
+    """,
 )
