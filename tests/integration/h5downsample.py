e7d19a446c25bcd98f6093aa639d11448bca77db
Rodrigo Garcia <rg3290@gadi-login-02.gadi.nci.org.au>
2020-11-11 18:41:17 +1100

    modification: code cleanup and include mndwi.h5 downsampling

--- tests/integration/h5downsample.py
+++ tests/integration/h5downsample.py
@@ -1,21 +1,32 @@
 """
 Make a HDF5 file much smaller by shrinking all embedded images.
 
-(Intended for creating test datasets where the pixels don't
-matter much but the structure does. The downsampling is dirty.)
+This script:
+1. overwrites the input data, and;
+2. is intended for creating test datasets where the pixels don't
+   matter much but the structure does. The downsampling is dirty.
 """
 import re
-from pathlib import Path
-from typing import List, Optional
-
-import click
 import h5py
+import click
+
 from click import secho
 from click import style
+from pathlib import Path
+from typing import List, Optional
 
 from eodatasets3.ui import PathPath
 from eodatasets3.wagl import find_a_granule_name
 
+
+try:
+    from sh import h5repack, gdal_translate
+except ImportError:
+    raise ImportError(
+        "Unable to import h5repack/gdal_translate. "
+        "Ensure Python's sh module is installed along "
+        "with h5tools and gdal-bin"
+    )
 
 def find_h5_paths(h5_obj: h5py.Group, dataset_class: str = "") -> List[str]:
     """
@@ -41,27 +52,73 @@ RES_GROUP_PATH = re.compile(r"(.*/RES-GROUP-\d+)/")
 @click.option("--factor", type=int, default=100)
 @click.option("--anti-alias/--no-anti-alias", is_flag=True, default=False)
 def downsample(input: Path, factor: int, anti_alias: bool):
-    # Fail early if h5repack cli command is not available.
-    from sh import h5repack, gdal_translate
 
     granule_name = find_a_granule_name(input)
     fmask_image = input.with_name(f"{granule_name}.fmask.img")
+    mndwi_image = input.with_name(f"{granule_name}.mndwi.h5")
 
-    nbar_size = None
-    with h5py.File(input) as f:
-        image_paths = find_h5_paths(f, "IMAGE")
+    # list of dataset names excluded from the downsampling
+    wagl_excl_dnames = ["NBAR", "NBART"]
+    mndwi_excl_dnames = [
+        "mndwi_image_LAMBERTIAN", "mndwi_image_LMBSKYG", "blue", "green", "red"
+    ]
 
-        for i, image_path in enumerate(image_paths):
-            old_image: Optional[h5py.Dataset] = f[image_path]
+    # list required dataset names
+    wagl_req_dnames = ["LMBSKYG"]
+    mndwi_req_dnames = ["mndwi_image_LMBADJ"]
 
-            def info(msg: str):
-                secho(
-                    f"{i: 4}/{len(image_paths)} {style(repr(image_path), fg='blue')}: {msg}"
-                )
+    # downsample mndwi.h5
+    secho(f"Scaling mndwi {mndwi_image}")
+    req_size = _downsample_h5_datasets(mndwi_image, factor, mndwi_excl_dnames, mndwi_req_dnames)
 
+    # downsample wagl.h5
+    secho(f"Scaling wagl {input}")
+    req_size = _downsample_h5_datasets(input, factor, wagl_excl_dnames, wagl_req_dnames)
+
+    if fmask_image.exists():
+        secho(f"Scaling fmask {fmask_image}")
+        tmp = fmask_image.with_suffix(f".tmp.{fmask_image.suffix}")
+        gdal_translate("-outsize", req_size[1], req_size[0], fmask_image, tmp)
+        tmp.rename(fmask_image)
+
+
+def _downsample_h5_datasets(h5_fname, factor, excl_dnames=[], req_dnames=[]):
+
+    with h5py.File(h5_fname, "r+") as fid:
+
+        image_paths = find_h5_paths(fid, "IMAGE")
+        npaths = len(image_paths)
+
+        # ----------------------------------------- #
+        #  assess if the required dataset(s) exist  #
+        # ----------------------------------------- #
+        if req_dnames:
+            absent_ds = []
+            for rq_dn in req_dnames:
+                if not any(rq_dn in img_path for img_path in image_paths):
+                    absent_ds.append(rq_dn)
+            if absent_ds:
+                raise ValueError("{0} image(s) not found".format(", ".join(absent_ds)))
+
+        # --------------------------------------------------- #
+        #  iterate and downscale, skipping excluded datasets  #
+        # --------------------------------------------------- #
+        req_size = None
+        for i, img_path in enumerate(image_paths):
+
+            if excl_dnames:
+                # delete excluded dataset names if specified
+                if any(ele in img_path for ele in excl_dnames):
+                    del fid[str(img_path)]
+                    continue
+
+            old_image: Optional[h5py.Dataset] = fid[img_path]
             old_shape = old_image.shape
+
+            info_str = f"{i: 4}/{npaths} {style(repr(img_path), fg='blue')}"
+
             if all(dim_size < factor for dim_size in old_shape):
-                info("Skipping")
+                secho(f"{info_str}: Skipping")
                 continue
 
             attrs = dict(old_image.attrs.items())
@@ -69,45 +126,45 @@ def downsample(input: Path, factor: int, anti_alias: bool):
 
             new_data = old_image[()][::factor, ::factor]
             new_shape = new_data.shape
-            info(f"New shape: {new_shape!r}")
+            secho(f"{info_str}: New shape: {new_shape!r}")
             del old_image
-            del f[str(image_path)]
+            del fid[str(img_path)]
 
-            folder, name = image_path.rsplit("/", 1)
-            parent: h5py.Group = f[str(folder)]
+            folder, name = img_path.rsplit("/", 1)
+            parent: h5py.Group = fid[str(folder)]
 
             image = parent.create_dataset(name, new_shape, data=new_data)
             new_geotransform = list(old_geotransform)
             new_geotransform[1] *= old_shape[1] / new_shape[1]
             new_geotransform[5] *= old_shape[0] / new_shape[0]
             attrs["geotransform"] = new_geotransform
+
+            # update resolution attr if it exists
+            res_key = [key for key in attrs.keys() if "resolution" in key]
+            if res_key:
+                attrs[res_key[0]] = abs(new_geotransform[5])
+
+            # update dataset attrs
             image.attrs.update(attrs)
 
             # Update any res group with the new resolution.
-            res_group_path = _get_res_group_path(image_path)
+            res_group_path = _get_res_group_path(img_path)
             if res_group_path:
-                res_group = f[res_group_path]
+                res_group = fid[res_group_path]
                 res_group.attrs["resolution"] = [
                     abs(new_geotransform[5]),
                     abs(new_geotransform[1]),
                 ]
 
-            if "/NBAR/" in image_path:
-                nbar_size = new_shape
+            if req_dnames:
+                if req_dnames[0] in img_path:
+                    req_size = new_shape
 
-    if nbar_size is None:
-        raise ValueError("No nbar image found?")
+    h5fn_repacked = h5_fname.with_suffix(".repacked.h5")
+    h5repack("-f", "GZIP=5", h5_fname, h5fn_repacked)
+    h5fn_repacked.rename(h5_fname)
 
-    # We need to repack the file to actually free up the space.
-    repacked = input.with_suffix(".repacked.h5")
-    h5repack("-f", "GZIP=5", input, repacked)
-    repacked.rename(input)
-
-    if fmask_image.exists():
-        secho(f"Scaling fmask {fmask_image}")
-        tmp = fmask_image.with_suffix(f".tmp.{fmask_image.suffix}")
-        gdal_translate("-outsize", nbar_size[1], nbar_size[0], fmask_image, tmp)
-        tmp.rename(fmask_image)
+    return req_size
 
 
 def _get_res_group_path(image_path: str) -> Optional[str]:
