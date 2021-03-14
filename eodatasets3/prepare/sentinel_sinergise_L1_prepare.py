a2c8cd1dcf5ae46ef162409f56668aca88654848
ohehir <aaron.ohehir@gmail.com>
2021-03-15 09:51:37 +1100

    formating.

--- eodatasets3/prepare/sentinel_sinergise_L1_prepare.py
+++ eodatasets3/prepare/sentinel_sinergise_L1_prepare.py
@@ -5,129 +5,221 @@ import json
 from os import listdir
 from os.path import isfile, join
 import sys
-
-path = '/g/data/up71/projects/index-testing-wagl/repo/eo-datasets/eodatasets3'
-sys.append(path)
-
+import uuid
+import click
+from typing import Dict, Tuple
 from eodatasets3 import DatasetAssembler
+from eodatasets3.ui import PathPath
 
-output_yaml_path = Path("/home/547/awo547/s2_delete_me.yaml")
-ds_path = Path("/home/547/awo547/sample_syn_s2/")
-metadata_xml_path = "/home/547/awo547/sample_syn_s2/metadata.xml"
-path = Path("/home/547/awo547/sample_syn_s2/B08.jp2")
-product_path = Path("/home/547/awo547/sample_syn_s2/productInfo.json")
-format_correctness_path = "/home/547/awo547/sample_syn_s2/qi/FORMAT_CORRECTNESS.xml"
+# python sentinel_sinergise_L1_prepare.py "/home/547/awo547/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446/productInfo.json" "/home/547/awo547/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446/metadata.xml" "/home/547/awo547/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446/qi/FORMAT_CORRECTNESS.xml" "/home/547/awo547/sin_s2am_level1_1-0-0_55HFA_2020-10-11.yaml" "/home/547/awo547/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446/"
+
+SENTINEL_MSI_BAND_ALIASES = {
+    "01": "coastal_aerosol",
+    "02": "blue",
+    "03": "green",
+    "04": "red",
+    "05": "red_edge_1",
+    "06": "red_edge_2",
+    "07": "red_edge_3",
+    "08": "nir_1",
+    "8A": "nir_2",
+    "09": "water_vapour",
+    "10": "swir_1_cirrus",
+    "11": "swir_2",
+    "12": "swir_3",
+}
 
 
-def extract_metadata_from_product_info(product_path):
-    fp = open(product_info)
+def extract_metadata_from_product_info(product_path: Path) -> Dict:
+    fp = open(product_path)
     product = json.loads(fp.read())
-    
-    synergise_product_name = product['name']
-    synergise_product_id = product['id']
-    timestamp = product['tiles'][0]['timestamp']
-    utm_zone = product['tiles'][0]['utmZone']
-    latitude_band = product['tiles'][0]['latitudeBand']
-    grid_square = product['tiles'][0]['gridSquare']
-    region_code = '%s%s%s' % (utm_zone, latitude_band, grid_square)
+
+    synergise_product_name = product["name"]
+    synergise_product_id = product["id"]
+    timestamp = product["tiles"][0]["timestamp"]
+    utm_zone = product["tiles"][0]["utmZone"]
+    latitude_band = product["tiles"][0]["latitudeBand"]
+    grid_square = product["tiles"][0]["gridSquare"]
+    region_code = "%s%s%s" % (utm_zone, latitude_band, grid_square)
 
     return {
-            'synergise_product_name': synergise_product_name,
-            'synergise_product_id': synergise_product_id,
-            'timestamp': timestamp,
-            'utm_zone': utm_zone,
-            'latitude_band': latitude_band,
-            'grid_square': grid_square,
-            'region_code': region_code,
-            }
+        "synergise_product_name": synergise_product_name,
+        "synergise_product_id": synergise_product_id,
+        "timestamp": timestamp,
+        "region_code": region_code,
+    }
 
 
-def extract_metadata_from_metadata_xml(metadata_xml_path):
+def extract_metadata_from_metadata_xml(metadata_xml_path: str) -> Dict:
     xmldoc = minidom.parse(metadata_xml_path)
 
-    cloud = float(xmldoc.getElementsByTagName('CLOUDY_PIXEL_PERCENTAGE')[0].firstChild.data)
-    downlink_priority = xmldoc.getElementsByTagName('DOWNLINK_PRIORITY')[0].firstChild.data
-    datastrip_id = xmldoc.getElementsByTagName('DATASTRIP_ID')[0].firstChild.data
-    solar_azimuth = float(xmldoc.getElementsByTagName("Mean_Sun_Angle")[0].getElementsByTagName("ZENITH_ANGLE")[0].firstChild.data)
-    solar_zenith = float(xmldoc.getElementsByTagName("Mean_Sun_Angle")[0].getElementsByTagName("AZIMUTH_ANGLE")[0].firstChild.data)
+    cloud = float(
+        xmldoc.getElementsByTagName("CLOUDY_PIXEL_PERCENTAGE")[0].firstChild.data
+    )
+    downlink_priority = xmldoc.getElementsByTagName("DOWNLINK_PRIORITY")[
+        0
+    ].firstChild.data
+    datastrip_id = xmldoc.getElementsByTagName("DATASTRIP_ID")[0].firstChild.data
+    solar_azimuth = float(
+        xmldoc.getElementsByTagName("Mean_Sun_Angle")[0]
+        .getElementsByTagName("ZENITH_ANGLE")[0]
+        .firstChild.data
+    )
+    solar_zenith = float(
+        xmldoc.getElementsByTagName("Mean_Sun_Angle")[0]
+        .getElementsByTagName("AZIMUTH_ANGLE")[0]
+        .firstChild.data
+    )
 
-    resolutions = xmldoc.getElementsByTagName('Size')
+    resolutions = xmldoc.getElementsByTagName("Size")
     r_list = []
-    for i in resolutions: r_list.append(int(i.attributes['resolution'].value))
+    for i in resolutions:
+        r_list.append(int(i.attributes["resolution"].value))
     resolution = min(r_list)
 
     return {
-            'cloud': cloud,
-            'downlink_priority': downlink_priority,
-            'datastrip_id': datastrip_id,
-            'solar_azimuth': solar_azimuth,
-            'solar_zenith': solar_zenith,
-            'resolution': resolution,
-            }
+        "cloud": cloud,
+        "downlink_priority": downlink_priority,
+        "datastrip_id": datastrip_id,
+        "solar_azimuth": solar_azimuth,
+        "solar_zenith": solar_zenith,
+        "resolution": resolution,
+    }
 
 
-def extract_metadata_from_format_correctness(format_correctness_path):
+def extract_metadata_from_format_correctness(format_correctness_path: str) -> Dict:
     xmldoc = minidom.parse(format_correctness_path)
 
-    source_system = xmldoc.getElementsByTagName('System')[0].firstChild.data
-    creator_version = xmldoc.getElementsByTagName('Creator_Version')[0].firstChild.data
-    creation_date = xmldoc.getElementsByTagName('Creation_Date')[0].firstChild.data
-    datastrip_metadata = xmldoc.getElementsByTagName('File_Name')[0].firstChild.data
+    source_system = xmldoc.getElementsByTagName("System")[0].firstChild.data
+    creator_version = xmldoc.getElementsByTagName("Creator_Version")[0].firstChild.data
+    creation_date = xmldoc.getElementsByTagName("Creation_Date")[0].firstChild.data
+    datastrip_metadata = xmldoc.getElementsByTagName("File_Name")[0].firstChild.data
 
     return {
-            'source_system': source_system,
-            'creator_version': creator_version,
-            'creation_date': creation_date,
-            'datastrip_metadata': datastrip_metadata,
-           }
+        "source_system": source_system,
+        "creator_version": creator_version,
+        "creation_date": creation_date,
+        "datastrip_metadata": datastrip_metadata,
+    }
 
 
-def prepare_and_write(output_yaml_path, ds_path, product_info, metadata_xml, format_correctness):
+def prepare_and_write(
+    output_yaml_path: Path,
+    ds_path: Path,
+    product_info: Dict,
+    metadata_xml: Dict,
+    format_correctness: Dict,
+) -> Tuple[uuid.UUID, Path]:
     with DatasetAssembler(
-        metadata_path=output_yaml_path,
-        dataset_location=ds_path,
+        metadata_path=output_yaml_path, dataset_location=ds_path
     ) as p:
-        p.datetime = timestamp # datetime(2017, 4, 5, 11, 17, 36) 2016-06-28 00:02:28.624635Z
-        p.properties["eo:instrument"] = 'MSI'
-        p.properties["eo:platform"] = 'sentinel-2a'
+        p.datetime = product_info["timestamp"]
+        p.properties["eo:instrument"] = "MSI"
+        p.properties["eo:platform"] = "sentinel-2a"
         p.properties["odc:dataset_version"] = "1.0.0"
-        p.properties["odc:producer"] = 'ga.gov.au'
+        p.properties["odc:producer"] = "sinergise.com"
         p.properties["odc:product_family"] = "level1"
-        p.properties["odc:processing_datetime"] = creation_date.split('=')[1].replace('T', ' ')
-        p.properties["eo:sun_elevation"] = solar_zenith
-        p.properties["eo:sun_azimuth"] = solar_azimuth
-        p.properties["eo:gsd"] = resolution
-        p.properties["eo:cloud_cover"] = cloud
-        p.properties["sentinel:sinergise_product_name"] = synergise_product_name
-        p.properties["sentinel:sinergise_product_id"] = synergise_product_id
-        p.properties["odc:file_format"] = 'JPEG2000'
-        p.properties["odc:region_code"] = region_code
+        p.properties["odc:processing_datetime"] = (
+            format_correctness["creation_date"].split("=")[1].replace("T", " ")
+        )
+        p.properties["eo:sun_elevation"] = metadata_xml["solar_zenith"]
+        p.properties["eo:sun_azimuth"] = metadata_xml["solar_azimuth"]
+        p.properties["eo:gsd"] = metadata_xml["resolution"]
+        p.properties["eo:cloud_cover"] = metadata_xml["cloud"]
+        p.properties["sentinel:sinergise_product_name"] = product_info[
+            "synergise_product_name"
+        ]
+        p.properties["sentinel:sinergise_product_id"] = product_info[
+            "synergise_product_id"
+        ]
+        p.properties["odc:file_format"] = "JPEG2000"
+        p.properties["odc:region_code"] = product_info["region_code"]
         p.properties["sentinel:data_type"] = "Level-1C"
         p.properties["sentinel:product_type"] = "S2MSI1C"
-        p.properties["sentinel:software_version"] = creator_version
-        p.properties["sentinel:source_system"] = source_system
-        p.properties["sentinel:datastrip_metadata"] = datastrip_metadata
-        p.properties["sentinel:downlink_priority"] = downlink_priority
-        p.properties["sentinel:datastrip_id"] = datastrip_id
-   
+        p.properties["sentinel:software_version"] = format_correctness[
+            "creator_version"
+        ]
+        p.properties["sentinel:source_system"] = format_correctness["source_system"]
+        p.properties["sentinel:datastrip_metadata"] = format_correctness[
+            "datastrip_metadata"
+        ]
+        p.properties["sentinel:downlink_priority"] = metadata_xml["downlink_priority"]
+        p.properties["sentinel:datastrip_id"] = metadata_xml["datastrip_id"]
+
         directory = [f for f in listdir(ds_path) if isfile(join(ds_path, f))]
 
         for dataset in directory:
-            if ('.jp2' in dataset and 'preview' not in dataset and 'TCI' not in dataset):
-                name = dataset.replace('.jp2', '').lower()
+            if ".jp2" in dataset and "preview" not in dataset and "TCI" not in dataset:
+                band = dataset.replace(".jp2", "").replace("B", "")
+                name = SENTINEL_MSI_BAND_ALIASES[band]
                 path = ds_path / dataset
-                print(name + ': ' + str(path))
                 p.note_measurement(path=path, name=name)
-   
-        p.done()
+
+        return p.done()
 
 
-main():
-    product_info = extract_metadata_from_product_info(product_path)
+@click.command(help=__doc__)
+@click.argument(
+    "product_path",
+    type=str,
+)
+@click.argument(
+    "metadata_xml_path",
+    type=str,
+)
+@click.argument(
+    "format_correctness_path",
+    type=str,
+)
+@click.argument(
+    "output_yaml_path",
+    type=str,
+)
+@click.argument(
+    "ds_path",
+    type=str,
+)
+def main(
+    product_path: str,
+    metadata_xml_path: str,
+    format_correctness_path: str,
+    output_yaml_path: str,
+    ds_path: str,
+):
+    product_info = extract_metadata_from_product_info(Path(product_path))
     metadata_xml = extract_metadata_from_metadata_xml(metadata_xml_path)
-    format_correctness = extract_metadata_from_format_correctness(format_correctness_path)
+    format_correctness = extract_metadata_from_format_correctness(
+        format_correctness_path
+    )
 
-    prepare_and_write(output_yaml_path, ds_path, product_info, metadata_xml, format_correctness)
+    uuid, path = prepare_and_write(
+        Path(output_yaml_path),
+        Path(ds_path),
+        product_info,
+        metadata_xml,
+        format_correctness,
+    )
+    return path
+
+
+def run(
+    product_path, metadata_xml_path, format_correctness_path, output_yaml_path, ds_path
+):
+
+    product_info = extract_metadata_from_product_info(Path(product_path))
+    metadata_xml = extract_metadata_from_metadata_xml(metadata_xml_path)
+    format_correctness = extract_metadata_from_format_correctness(
+        format_correctness_path
+    )
+    uuid, path = prepare_and_write(
+        Path(output_yaml_path),
+        Path(ds_path),
+        product_info,
+        metadata_xml,
+        format_correctness,
+    )
+    return path
+
 
 if __name__ == "__main__":
     main()
--- tests/integration/prepare/test_prepare_sinergise_sentinel_l1.py
+++ tests/integration/prepare/test_prepare_sinergise_sentinel_l1.py
@@ -1,4 +1,4 @@
-#from eodatasets3.prepare import sentinel_sinergise_L1_prepare
+# from eodatasets3.prepare import sentinel_sinergise_L1_prepare
 from pathlib import Path
 from pprint import pprint
 import pytest
@@ -7,123 +7,155 @@ import yaml
 import datetime
 
 import sys
+
 sys.path.append("/g/data/up71/projects/index-testing-wagl/repo/eo-datasets/tests")
 sys.path.append("/g/data/up71/projects/index-testing-wagl/repo/eo-datasets/eodatasets3")
-sys.path.append("/g/data/up71/projects/index-testing-wagl/repo/eo-datasets/eodatasets3/prepare")
-
-from common import check_prepare_outputs    # Change to tests.common
-from common import run_prepare_cli          # Change to tests.common
-
-from prepare import sentinel_sinergise_L1_prepare # eodatasets3.prepare
-#from sentinel_sinergise_L1_prepare import run
-
-path = "data/sinergise_s2_l1c/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446"
-
-METADATA_XML_PATH: Path = (
-    Path(__file__).parent.parent
-    / path / "metadata.xml"
+sys.path.append(
+    "/g/data/up71/projects/index-testing-wagl/repo/eo-datasets/eodatasets3/prepare"
 )
 
-PRODUCT_INFO_PATH: Path = (
-    Path(__file__).parent.parent
-    / path / "productInfo.json"
+from common import check_prepare_outputs  # Change to tests.common
+from common import run_prepare_cli  # Change to tests.common
+
+from prepare import sentinel_sinergise_L1_prepare  # eodatasets3.prepare
+
+# from sentinel_sinergise_L1_prepare import run
+
+path = (
+    "data/sinergise_s2_l1c/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446"
 )
+
+METADATA_XML_PATH: Path = Path(__file__).parent.parent / path / "metadata.xml"
+
+PRODUCT_INFO_PATH: Path = Path(__file__).parent.parent / path / "productInfo.json"
 
 FORMAT_CORRECTNESS_PATH: Path = (
-    Path(__file__).parent.parent
-    / path / "qi/FORMAT_CORRECTNESS.xml"
+    Path(__file__).parent.parent / path / "qi/FORMAT_CORRECTNESS.xml"
 )
 
-DATASET_DIR: Path = (
-    Path(__file__).parent.parent
-    / path
-)
+DATASET_DIR: Path = Path(__file__).parent.parent / path
 
 
 @pytest.fixture()
 def expected_dataset_document():
-    return  {'$schema': 'https://schemas.opendatacube.org/dataset',
- 'accessories': {},
- 'crs': 'epsg:32755',
- 'geometry': {'coordinates': [[[600332.7272727273, 6100000.0],
-                               [709800.0, 6100000.0],
-                               [709800.0, 5990200.0],
-                               [600000.0, 5990200.0],
-                               [600000.0, 6099500.909090909],
-                               [600000.0, 6100000.0],
-                               [600332.7272727273, 6100000.0]]],
-              'type': 'Polygon'},
- 'grids': {'default': {'shape': [55, 55],
-                       'transform': [1996.3636363636363,
-                                     0.0,
-                                     600000.0,
-                                     0.0,
-                                     -1996.3636363636363,
-                                     6100000.0,
-                                     0.0,
-                                     0.0,
-                                     1.0]},
-           'green_nir_1_blue_red': {'shape': [110, 110],
-                                    'transform': [998.1818181818181,
-                                                  0.0,
-                                                  600000.0,
-                                                  0.0,
-                                                  -998.1818181818181,
-                                                  6100000.0,
-                                                  0.0,
-                                                  0.0,
-                                                  1.0]},
-           'water_vapour_swir_1_cirrus_coastal_aerosol': {'shape': [19, 19],
-                                                          'transform': [5778.9473684210525,
-                                                                        0.0,
-                                                                        600000.0,
-                                                                        0.0,
-                                                                        -5778.9473684210525,
-                                                                        6100000.0,
-                                                                        0.0,
-                                                                        0.0,
-                                                                        1.0]}},
- 'label': 'sin_s2am_level1_1-0-0_55HFA_2020-10-11',
- 'lineage': {},
- 'measurements': {'blue': {'grid': 'green_nir_1_blue_red', 'path': 'B02.jp2'},
-                  'coastal_aerosol': {'grid': 'water_vapour_swir_1_cirrus_coastal_aerosol',
-                                      'path': 'B01.jp2'},
-                  'green': {'grid': 'green_nir_1_blue_red', 'path': 'B03.jp2'},
-                  'nir_1': {'grid': 'green_nir_1_blue_red', 'path': 'B08.jp2'},
-                  'nir_2': {'path': 'B8A.jp2'},
-                  'red': {'grid': 'green_nir_1_blue_red', 'path': 'B04.jp2'},
-                  'red_edge_1': {'path': 'B05.jp2'},
-                  'red_edge_2': {'path': 'B06.jp2'},
-                  'red_edge_3': {'path': 'B07.jp2'},
-                  'swir_1_cirrus': {'grid': 'water_vapour_swir_1_cirrus_coastal_aerosol',
-                                    'path': 'B10.jp2'},
-                  'swir_2': {'path': 'B11.jp2'},
-                  'swir_3': {'path': 'B12.jp2'},
-                  'water_vapour': {'grid': 'water_vapour_swir_1_cirrus_coastal_aerosol',
-                                   'path': 'B09.jp2'}},
- 'product': {'name': 'sin_s2am_level1_1'},
- 'properties': {'datetime': datetime.datetime(2020, 10, 11, 0, 6, 49, 882000, tzinfo=datetime.timezone.utc),
-                'eo:cloud_cover': 24.9912,
-                'eo:gsd': 10,
-                'eo:instrument': 'MSI',
-                'eo:platform': 'sentinel-2a',
-                'eo:sun_azimuth': 37.3713908882192,
-                'eo:sun_elevation': 46.3307328858312,
-                'odc:dataset_version': '1.0.0',
-                'odc:file_format': 'JPEG2000',
-                'odc:processing_datetime': datetime.datetime(2020, 10, 11, 1, 47, 21, tzinfo=datetime.timezone.utc),
-                'odc:producer': 'sinergise.com',
-                'odc:product_family': 'level1',
-                'odc:region_code': '55HFA',
-                'sentinel:data_type': 'Level-1C',
-                'sentinel:datastrip_id': 'S2B_OPER_MSI_L1C_DS_EPAE_20201011T011446_S20201011T000244_N02.09',
-                'sentinel:datastrip_metadata': 'GRANULE/L1C_T55HFA_A018789_20201011T000244/QI_DATA/FORMAT_CORRECTNESS.xml',
-                'sentinel:downlink_priority': 'NOMINAL',
-                'sentinel:product_type': 'S2MSI1C',
-                'sentinel:sinergise_product_id': '73e1a409-595d-4fbf-8fe0-01e0ee26bf00',
-                'sentinel:sinergise_product_name': 'S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446',
-                'sentinel:software_version': '01.06.04',
-                'sentinel:source_system': 'OLQC-SC'}}
+    return {
+        "$schema": "https://schemas.opendatacube.org/dataset",
+        "accessories": {},
+        "crs": "epsg:32755",
+        "geometry": {
+            "coordinates": [
+                [
+                    [600332.7272727273, 6100000.0],
+                    [709800.0, 6100000.0],
+                    [709800.0, 5990200.0],
+                    [600000.0, 5990200.0],
+                    [600000.0, 6099500.909090909],
+                    [600000.0, 6100000.0],
+                    [600332.7272727273, 6100000.0],
+                ]
+            ],
+            "type": "Polygon",
+        },
+        "grids": {
+            "default": {
+                "shape": [55, 55],
+                "transform": [
+                    1996.3636363636363,
+                    0.0,
+                    600000.0,
+                    0.0,
+                    -1996.3636363636363,
+                    6100000.0,
+                    0.0,
+                    0.0,
+                    1.0,
+                ],
+            },
+            "green_nir_1_blue_red": {
+                "shape": [110, 110],
+                "transform": [
+                    998.1818181818181,
+                    0.0,
+                    600000.0,
+                    0.0,
+                    -998.1818181818181,
+                    6100000.0,
+                    0.0,
+                    0.0,
+                    1.0,
+                ],
+            },
+            "water_vapour_swir_1_cirrus_coastal_aerosol": {
+                "shape": [19, 19],
+                "transform": [
+                    5778.9473684210525,
+                    0.0,
+                    600000.0,
+                    0.0,
+                    -5778.9473684210525,
+                    6100000.0,
+                    0.0,
+                    0.0,
+                    1.0,
+                ],
+            },
+        },
+        "label": "sin_s2am_level1_1-0-0_55HFA_2020-10-11",
+        "lineage": {},
+        "measurements": {
+            "blue": {"grid": "green_nir_1_blue_red", "path": "B02.jp2"},
+            "coastal_aerosol": {
+                "grid": "water_vapour_swir_1_cirrus_coastal_aerosol",
+                "path": "B01.jp2",
+            },
+            "green": {"grid": "green_nir_1_blue_red", "path": "B03.jp2"},
+            "nir_1": {"grid": "green_nir_1_blue_red", "path": "B08.jp2"},
+            "nir_2": {"path": "B8A.jp2"},
+            "red": {"grid": "green_nir_1_blue_red", "path": "B04.jp2"},
+            "red_edge_1": {"path": "B05.jp2"},
+            "red_edge_2": {"path": "B06.jp2"},
+            "red_edge_3": {"path": "B07.jp2"},
+            "swir_1_cirrus": {
+                "grid": "water_vapour_swir_1_cirrus_coastal_aerosol",
+                "path": "B10.jp2",
+            },
+            "swir_2": {"path": "B11.jp2"},
+            "swir_3": {"path": "B12.jp2"},
+            "water_vapour": {
+                "grid": "water_vapour_swir_1_cirrus_coastal_aerosol",
+                "path": "B09.jp2",
+            },
+        },
+        "product": {"name": "sin_s2am_level1_1"},
+        "properties": {
+            "datetime": datetime.datetime(
+                2020, 10, 11, 0, 6, 49, 882000, tzinfo=datetime.timezone.utc
+            ),
+            "eo:cloud_cover": 24.9912,
+            "eo:gsd": 10,
+            "eo:instrument": "MSI",
+            "eo:platform": "sentinel-2a",
+            "eo:sun_azimuth": 37.3713908882192,
+            "eo:sun_elevation": 46.3307328858312,
+            "odc:dataset_version": "1.0.0",
+            "odc:file_format": "JPEG2000",
+            "odc:processing_datetime": datetime.datetime(
+                2020, 10, 11, 1, 47, 21, tzinfo=datetime.timezone.utc
+            ),
+            "odc:producer": "sinergise.com",
+            "odc:product_family": "level1",
+            "odc:region_code": "55HFA",
+            "sentinel:data_type": "Level-1C",
+            "sentinel:datastrip_id": "S2B_OPER_MSI_L1C_DS_EPAE_20201011T011446_S20201011T000244_N02.09",
+            "sentinel:datastrip_metadata": "GRANULE/L1C_T55HFA_A018789_20201011T000244/QI_DATA/FORMAT_CORRECTNESS.xml",
+            "sentinel:downlink_priority": "NOMINAL",
+            "sentinel:product_type": "S2MSI1C",
+            "sentinel:sinergise_product_id": "73e1a409-595d-4fbf-8fe0-01e0ee26bf00",
+            "sentinel:sinergise_product_name": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446",
+            "sentinel:software_version": "01.06.04",
+            "sentinel:source_system": "OLQC-SC",
+        },
+    }
 
 
 def test_run(tmp_path, expected_dataset_document):
@@ -142,7 +174,7 @@ def test_run(tmp_path, expected_dataset_document):
 
     # WHEN:
     #     I run prepare on that folder
-    output_yaml_path = outdir / 'test.yaml'
+    output_yaml_path = outdir / "test.yaml"
 
     run_prepare_cli(
         sentinel_sinergise_L1_prepare.main,
@@ -150,7 +182,7 @@ def test_run(tmp_path, expected_dataset_document):
         METADATA_XML_PATH,
         FORMAT_CORRECTNESS_PATH,
         output_yaml_path,
-        outdir
+        outdir,
     )
 
     # THEN
