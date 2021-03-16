2bd4b5d8a67d2d19aea94e04af7d049f93be8944
ohehir <aaron.ohehir@gmail.com>
2021-03-16 14:08:38 +1100

    modified:   test_prepare_sinergise_sentinel_l1.py

--- tests/integration/prepare/test_prepare_sinergise_sentinel_l1.py
+++ tests/integration/prepare/test_prepare_sinergise_sentinel_l1.py
@@ -179,4 +179,6 @@ def test_run(tmp_path, expected_dataset_document):
     with output_yaml_path.open("r") as f:
         generated_doc = yaml.safe_load(f)
         del generated_doc["id"]
+    from pprint import pprint
+    pprint(generated_doc)
     assert expected_dataset_document == generated_doc
