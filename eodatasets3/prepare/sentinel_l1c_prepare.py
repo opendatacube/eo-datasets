b0618acadb22de9e7ed7bfc734cd90530c0c9012
Jeremy Hooke <jez@stulk.com>
2021-04-12 12:05:06 +1000

    Check for more xml extraction errors
    Raise exceptions when children don't match expected. Otherwise
    we face silent errors.

--- eodatasets3/prepare/sentinel_l1c_prepare.py
+++ eodatasets3/prepare/sentinel_l1c_prepare.py
@@ -8,7 +8,6 @@ import zipfile
 from pathlib import Path
 from typing import Tuple, Dict
 from xml.dom import minidom
-
 import click
 
 from eodatasets3 import DatasetAssembler
@@ -33,135 +32,123 @@ SENTINEL_MSI_BAND_ALIASES = {
 
 def process_product_info(product_path: Path) -> Dict:
     with product_path.open() as fp:
-        product = json.loads(fp.read())
+        product = json.load(fp)
 
-    synergise_product_name = product["name"]
-    synergise_product_id = product["id"]
     if len(product["tiles"]) > 1:
         raise NotImplementedError("No support for multi-tiled products yet")
-    timestamp = product["tiles"][0]["timestamp"]
-    utm_zone = product["tiles"][0]["utmZone"]
-    latitude_band = product["tiles"][0]["latitudeBand"]
-    grid_square = product["tiles"][0]["gridSquare"]
-    region_code = "%s%s%s" % (utm_zone, latitude_band, grid_square)
+    tile = product["tiles"][0]
 
+    utm_zone = tile["utmZone"]
+    latitude_band = tile["latitudeBand"]
+    grid_square = tile["gridSquare"]
     return {
-        "sinergise_product_name": synergise_product_name,
-        "sinergise_product_id": synergise_product_id,
-        "datetime": timestamp,
-        "odc:region_code": region_code,
+        "sinergise_product_name": product["name"],
+        "sinergise_product_id": product["id"],
+        "datetime": tile["timestamp"],
+        "odc:region_code": "%s%s%s" % (utm_zone, latitude_band, grid_square),
         "sentinel:utm_zone": utm_zone,
         "sentinel:latitude_band": latitude_band,
         "sentinel:grid_square": grid_square,
     }
 
 
+def _node_name(el):
+    """Get a human-readable path to the given xml node"""
+    branch = [el]
+    while branch[-1].parentNode is not None:
+        branch.append(branch[-1].parentNode)
+
+    branch.reverse()
+    return "/".join(n.localName for n in branch[1:])
+
+
+def _value(root, *tags: str, type_=None):
+    """Get the text contents of a tag in an xml document.
+
+    Takes the tag name(s) to search for.
+    """
+    el = root
+
+    for tag in tags:
+        found = el.getElementsByTagName(tag)
+        if not found:
+            raise ValueError(f"Element not found in document: {tags!r}")
+        if len(found) > 1:
+            vs = "\n\t".join([_node_name(e) for e in found])
+            raise ValueError(f"Multiple matches found for tag: {tags!r}: \n\t{vs}")
+
+        el = found[0]
+
+    el.normalize()
+    if not len(el.childNodes) == 1:
+        raise NotImplementedError(
+            f"Not yet supported: Multiple child tags found for {tags!r}"
+        )
+    value = el.firstChild.data.strip()
+
+    if not value:
+        raise ValueError(f"Empty value for document at {tags!r}")
+
+    if type_ is not None:
+        value = type_(value)
+    return value
+
+
 def process_metadata_xml(metadata_xml_path: Path) -> Dict:
-    xmldoc = minidom.parse(str(metadata_xml_path))
+    root = minidom.parse(str(metadata_xml_path))
 
-    cloud = float(
-        xmldoc.getElementsByTagName("CLOUDY_PIXEL_PERCENTAGE")[0].firstChild.data
+    resolution = min(
+        int(i.attributes["resolution"].value) for i in root.getElementsByTagName("Size")
     )
-
-    datastrip_id = xmldoc.getElementsByTagName("DATASTRIP_ID")[0].firstChild.data
-    tile_id = xmldoc.getElementsByTagName("TILE_ID")[0].firstChild.data
-    solar_azimuth = float(
-        xmldoc.getElementsByTagName("Mean_Sun_Angle")[0]
-        .getElementsByTagName("ZENITH_ANGLE")[0]
-        .firstChild.data
-    )
-    solar_zenith = float(
-        xmldoc.getElementsByTagName("Mean_Sun_Angle")[0]
-        .getElementsByTagName("AZIMUTH_ANGLE")[0]
-        .firstChild.data
-    )
-
-    processed_time = xmldoc.getElementsByTagName("ARCHIVING_TIME")[0].firstChild.data
-
-    resolutions = xmldoc.getElementsByTagName("Size")
-    r_list = []
-    for i in resolutions:
-        r_list.append(int(i.attributes["resolution"].value))
-    resolution = min(r_list)
     return {
-        "eo:cloud_cover": cloud,
-        "sentinel:datastrip_id": datastrip_id,
-        "sentinel:sentinel_tile_id": tile_id,
-        "eo:sun_azimuth": solar_azimuth,
-        "eo:sun_elevation": solar_zenith,
+        "eo:cloud_cover": _value(root, "CLOUDY_PIXEL_PERCENTAGE", type_=float),
+        "sentinel:datastrip_id": _value(root, "DATASTRIP_ID"),
+        "sentinel:sentinel_tile_id": _value(root, "TILE_ID"),
+        "eo:sun_azimuth": _value(root, "Mean_Sun_Angle", "ZENITH_ANGLE", type_=float),
+        "eo:sun_elevation": _value(
+            root, "Mean_Sun_Angle", "AZIMUTH_ANGLE", type_=float
+        ),
         "eo:gsd": resolution,
-        "odc:processing_datetime": processed_time,
+        "odc:processing_datetime": _value(root, "ARCHIVING_TIME"),
     }
 
 
 def process_mtd_ds(contents: str) -> Dict:
-    xmldoc = minidom.parseString(contents)
+    root = minidom.parseString(contents)
 
-    reception_station = xmldoc.getElementsByTagName("RECEPTION_STATION")[
-        0
-    ].firstChild.data
-    processing_center = xmldoc.getElementsByTagName("PROCESSING_CENTER")[
-        0
-    ].firstChild.data
-
-    resolutions = xmldoc.getElementsByTagName("RESOLUTION")
-    r_list = []
-    for i in resolutions:
-        r_list.append(int(i.firstChild.data))
-    resolution = min(r_list)
-
+    resolution = min(
+        int(i.firstChild.data) for i in root.getElementsByTagName("RESOLUTION")
+    )
     return {
-        "sentinel:reception_station": reception_station,
-        "sentinel:processing_center": processing_center,
+        "sentinel:reception_station": _value(root, "RECEPTION_STATION"),
+        "sentinel:processing_center": _value(root, "PROCESSING_CENTER"),
         "eo:gsd": resolution,
     }
 
 
 def process_mtd_tl(contents: str) -> Dict:
-    xmldoc = minidom.parseString(contents)
-
-    sun_azimuth = xmldoc.getElementsByTagName("AZIMUTH_ANGLE")[0].firstChild.data
-    sun_elevation = xmldoc.getElementsByTagName("ZENITH_ANGLE")[0].firstChild.data
-    datastrip_id = xmldoc.getElementsByTagName("DATASTRIP_ID")[0].firstChild.data
-
+    root = minidom.parseString(contents)
     return {
-        "eo:sun_azimuth": sun_azimuth,
-        "eo:sun_elevation": sun_elevation,
-        "sentinel:datastrip_id": datastrip_id,
+        "eo:sun_azimuth": _value(root, "Mean_Sun_Angle", "AZIMUTH_ANGLE"),
+        "eo:sun_elevation": _value(root, "Mean_Sun_Angle", "ZENITH_ANGLE"),
+        "sentinel:datastrip_id": _value(root, "DATASTRIP_ID"),
     }
 
 
 def process_mtd_msil1c(contents: str) -> Dict:
-    xmldoc = minidom.parseString(contents)
+    root = minidom.parseString(contents)
 
-    product_uri = xmldoc.getElementsByTagName("PRODUCT_URI")[0].firstChild.data
-    platform = xmldoc.getElementsByTagName("SPACECRAFT_NAME")[0].firstChild.data
-    orbit = xmldoc.getElementsByTagName("SENSING_ORBIT_NUMBER")[0].firstChild.data
-    orbit_direction = xmldoc.getElementsByTagName("SENSING_ORBIT_DIRECTION")[
-        0
-    ].firstChild.data
-    datatake_type = xmldoc.getElementsByTagName("DATATAKE_TYPE")[0].firstChild.data
-    processing_datetime = xmldoc.getElementsByTagName("GENERATION_TIME")[
-        0
-    ].firstChild.data
-    processing_baseline = xmldoc.getElementsByTagName("PROCESSING_BASELINE")[
-        0
-    ].firstChild.data
-    datetime = xmldoc.getElementsByTagName("PRODUCT_START_TIME")[0].firstChild.data
-    cloud_cover = xmldoc.getElementsByTagName("Cloud_Coverage_Assessment")[
-        0
-    ].firstChild.data
+    product_uri = _value(root, "PRODUCT_URI")
     region_code = product_uri.split("_")[5][1:]
-
     return {
-        "eo:platform": platform,
-        "sat:relative_orbit": int(orbit),
-        "sat:orbit_state": orbit_direction.lower(),
-        "sentinel:datatake_type": datatake_type,
-        "odc:processing_datetime": processing_datetime,
-        "sentinel:processing_baseline": processing_baseline,
-        "datetime": datetime,
-        "eo:cloud_cover": cloud_cover,
+        "eo:platform": _value(root, "SPACECRAFT_NAME"),
+        "sat:relative_orbit": _value(root, "SENSING_ORBIT_NUMBER", type_=int),
+        "sat:orbit_state": _value(root, "SENSING_ORBIT_DIRECTION").lower(),
+        "sentinel:datatake_type": _value(root, "DATATAKE_TYPE"),
+        "odc:processing_datetime": _value(root, "GENERATION_TIME"),
+        "sentinel:processing_baseline": _value(root, "PROCESSING_BASELINE"),
+        "datetime": _value(root, "PRODUCT_START_TIME"),
+        "eo:cloud_cover": _value(root, "Cloud_Coverage_Assessment"),
         "odc:region_code": region_code,
     }
 
