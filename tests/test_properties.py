cdaa3eb69b8d58383ae8cafe9feb73a82aac5f38
Jeremy Hooke <jez@stulk.com>
2021-05-14 12:26:33 +1000

    Allow multi-platform naming conventions

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -1,4 +1,6 @@
-from eodatasets3.model import DatasetDoc
+import pytest
+
+from eodatasets3.model import DatasetDoc, ComplicatedNamingConventions
 
 
 def test_multi_platform_fields():
@@ -23,3 +25,46 @@ def test_multi_platform_fields():
     d = DatasetDoc()
     d.platform = ""
     assert d.platform is None
+
+
+def test_naming_abbreviations():
+    d = DatasetDoc()
+    names = ComplicatedNamingConventions(d)
+
+    assert names.platform_abbreviated is None
+
+    # A single platform uses its known abbreviation.
+    d.platforms = ["landsat-5"]
+    assert names.platform_abbreviated == "ls5"
+
+    # Multiple platforms from a known group use the group name.
+    d.platforms = ["landsat-5", "landsat_7"]
+    assert names.platform_abbreviated == "ls"
+    d.platforms = ["sentinel-2a", "sentinel-2b"]
+    assert names.platform_abbreviated == "s2"
+
+    # Non-groupable platforms are dash-separated.
+    d.platforms = ["landsat-5", "sentinel-2a"]
+    assert names.platform_abbreviated is None
+
+
+def test_unknown_abbreviations():
+    d = DatasetDoc()
+    names = ComplicatedNamingConventions(d)
+
+    # Unknown platforms are abbreviated by just removing dashes.
+    d.platform = "grover-1"
+    assert names.platform_abbreviated == "grover1"
+
+    # Constellation can be used as a fallback grouping.
+    d.platforms = ["clippings-1a", "clippings-2b"]
+    d.properties["constellation"] = "clippings"
+    assert names.platform_abbreviated == "clippings"
+
+    # Unless unknown platforms aren't allowed
+    # (DEA wants to be stricter and add real abbreviations for everything.)
+    names = ComplicatedNamingConventions.for_standard_dea(d)
+    with pytest.raises(
+        ValueError, match="don't know the DEA abbreviation for platform"
+    ):
+        print(names.platform_abbreviated)
