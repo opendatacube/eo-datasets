57d4afc32180a7a4819726703e01d2ce90b7b1c5
Jeremy Hooke <jez@stulk.com>
2021-06-15 10:24:41 +1000

    Naming API consistency, add override test

--- eodatasets3/__init__.py
+++ eodatasets3/__init__.py
@@ -4,10 +4,20 @@ from __future__ import absolute_import
 
 from ._version import get_versions
 from .assemble import DatasetAssembler, IfExists
+from .properties import Eo3Properties
+
+from .names import convention as naming_convention
 
 REPO_URL = "https://github.com/GeoscienceAustralia/eo-datasets.git"
 
 __version__ = get_versions()["version"]
 del get_versions
 
-__all__ = (DatasetAssembler, IfExists, REPO_URL, __version__)
+__all__ = (
+    DatasetAssembler,
+    IfExists,
+    Eo3Properties,
+    naming_convention,
+    REPO_URL,
+    __version__,
+)
