215e31734af42a24a75467b4ae2ca2d9abeacb1b
Sai Ma <Sai.Ma@ga.gov.au>
2021-07-23 05:22:10 +0000

    Add default grid to STAC metadata

--- eodatasets3/stac.py
+++ eodatasets3/stac.py
@@ -100,7 +100,7 @@ def _proj_fields(grid: Dict[str, GridDoc], grid_name: str = "default") -> Dict:
     """
     Add fields of the STAC Projection (proj) Extension to a STAC Item
     """
-    grid_doc = grid.get(grid_name)
+    grid_doc = grid.get(grid_name or "default")
     if grid_doc:
         return {
             "proj:shape": grid_doc.shape,
