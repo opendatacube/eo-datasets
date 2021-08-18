b2edd97e40bb233b11b75a2c10a61672b0afe94c
Jeremy Hooke <jez@stulk.com>
2021-08-18 11:12:33 +1000

    Add a doc diff fallback
    We keep finding new variations not handled. They've
    changed the api a bit. So fall back to just printing
    the raw return.

--- tests/common.py
+++ tests/common.py
@@ -183,6 +183,9 @@ def format_doc_diffs(left: Dict, right: Dict) -> Iterable[str]:
         for offset in doc_diffs.tree["dictionary_item_removed"].items:
             offset: DiffLevel
             out.append(f"    {clean_offset(offset.path())} = {repr(offset.t1)}")
+    # Anything we missed from the (sometimes changing) diff api?
+    if len(out) == 1:
+        out.append(repr(doc_diffs))
 
     # If pytest verbose:
     out.extend(("Full output document: ", repr(left)))
