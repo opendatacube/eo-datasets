749ee56e35b3619e5f4d30e44ce5700d4ed47f72
Jeremy Hooke <jez@stulk.com>
2022-04-01 11:26:11 +1100

    Combine and parameterise S2 test types

---
+++ tests/integration/prepare/test_prepare_sentinel_l1.py
@@ -0,0 +1,358 @@
+import datetime
+import shutil
+from pathlib import Path
+
+import pytest
+
+from eodatasets3.prepare import sentinel_l1_prepare
+
+from tests.common import check_prepare_outputs
+
+ESA_INPUT_DATASET: Path = Path(__file__).parent.parent / (
+    "data/esa_s2_l1c/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.zip"
+)
+SINERGISE_INPUT_DATASET: Path = Path(__file__).parent.parent / (
+    "data/sinergise_s2_l1c/S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446"
+)
+
+
+ESA_EXPECTED_METADATA = {
+    "$schema": "https://schemas.opendatacube.org/dataset",
+    "id": "7c1df12c-e580-5fa2-b51b-c30a59e73bbf",
+    "crs": "epsg:32755",
+    "geometry": {
+        "coordinates": [
+            [
+                [600300.0, 6100000.0],
+                [709800.0, 6100000.0],
+                [709800.0, 5990200.0],
+                [600000.0, 5990200.0],
+                [600000.0, 6099700.0],
+                [600000.0, 6100000.0],
+                [600300.0, 6100000.0],
+            ]
+        ],
+        "type": "Polygon",
+    },
+    "grids": {
+        "300": {
+            "shape": [366, 366],
+            "transform": [
+                300.0,
+                0.0,
+                600000.0,
+                0.0,
+                -300.0,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+        "default": {
+            "shape": [1098, 1098],
+            "transform": [
+                100.0,
+                0.0,
+                600000.0,
+                0.0,
+                -100.0,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+        "50": {
+            "shape": [2196, 2196],
+            "transform": [
+                50.0,
+                0.0,
+                600000.0,
+                0.0,
+                -50.0,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+    },
+    "label": "esa_s2bm_level1_1-0-20201011_55HFA_2020-10-11",
+    "lineage": {},
+    "measurements": {
+        "blue": {
+            "grid": "50",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B02.jp2",
+        },
+        "coastal_aerosol": {
+            "grid": "300",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B01.jp2",
+        },
+        "green": {
+            "grid": "50",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B03.jp2",
+        },
+        "nir_1": {
+            "grid": "50",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B08.jp2",
+        },
+        "red": {
+            "grid": "50",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B04.jp2",
+        },
+        "red_edge_1": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B05.jp2"
+        },
+        "red_edge_2": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B06.jp2"
+        },
+        "red_edge_3": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B07.jp2"
+        },
+        "swir_1_cirrus": {
+            "grid": "300",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B10.jp2",
+        },
+        "swir_2": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B11.jp2"
+        },
+        "swir_3": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B12.jp2"
+        },
+        "water_vapour": {
+            "grid": "300",
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/IMG_DATA/T55HFA_20201011T000249_B09.jp2",
+        },
+    },
+    "product": {"name": "esa_s2bm_level1_1"},
+    "properties": {
+        "datetime": datetime.datetime(2020, 10, 11, 0, 6, 49, 882566),
+        "eo:cloud_cover": 24.9912,
+        "eo:gsd": 10,
+        "eo:instrument": "MSI",
+        "eo:platform": "sentinel-2b",
+        "eo:constellation": "sentinel-2",
+        "eo:sun_azimuth": 46.3307328858312,
+        "eo:sun_elevation": 37.3713908882192,
+        "odc:dataset_version": "1.0.20201011",
+        "odc:file_format": "JPEG2000",
+        "odc:processing_datetime": datetime.datetime(2020, 10, 11, 1, 47, 4, 112949),
+        "odc:producer": "esa.int",
+        "odc:product_family": "level1",
+        "odc:region_code": "55HFA",
+        "sentinel:datastrip_id": "S2B_OPER_MSI_L1C_DS_EPAE_20201011T011446_S20201011T000244_N02.09",
+        "sentinel:sentinel_tile_id": "S2B_OPER_MSI_L1C_TL_EPAE_20201011T011446_A018789_T55HFA_N02.09",
+        "sentinel:product_name": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446",
+        "sentinel:datatake_type": "INS-NOBS",
+        "sat:orbit_state": "descending",
+        "sat:relative_orbit": 30,
+        "sentinel:datatake_start_datetime": datetime.datetime(2020, 10, 11, 1, 14, 46),
+        "sentinel:processing_baseline": "02.09",
+        "sentinel:processing_center": "EPAE",
+        "sentinel:reception_station": "EDRS",
+    },
+    "accessories": {
+        "metadata:s2_datastrip": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/DATASTRIP/"
+            "DS_EPAE_20201011T011446_S20201011T000244/MTD_DS.xml"
+        },
+        "metadata:s2_user_product": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/MTD_MSIL1C.xml"
+        },
+        "metadata:s2_tile": {
+            "path": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.SAFE/GRANULE/"
+            "L1C_T55HFA_A018789_20201011T000244/MTD_TL.xml"
+        },
+    },
+}
+
+
+SINERGISE_EXPECTED_METADATA = {
+    "$schema": "https://schemas.opendatacube.org/dataset",
+    "id": "f3e0eee1-573c-5035-870e-8d8392df8e33",
+    "crs": "epsg:32755",
+    "product": {
+        "name": "sinergise_s2bm_level1_1",
+    },
+    "geometry": {
+        "coordinates": [
+            [
+                [600000.0, 5990200.0],
+                [600000.0, 6099500.909090909],
+                [600000.0, 6100000.0],
+                [600332.7272727273, 6100000.0],
+                [709800.0, 6100000.0],
+                [709800.0, 5990200.0],
+                [600000.0, 5990200.0],
+            ]
+        ],
+        "type": "Polygon",
+    },
+    "grids": {
+        "default": {
+            "shape": [55, 55],
+            "transform": [
+                1996.3636363636363,
+                0.0,
+                600000.0,
+                0.0,
+                -1996.3636363636363,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+        "998": {
+            "shape": [110, 110],
+            "transform": [
+                998.1818181818181,
+                0.0,
+                600000.0,
+                0.0,
+                -998.1818181818181,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+        "5778": {
+            "shape": [19, 19],
+            "transform": [
+                5778.9473684210525,
+                0.0,
+                600000.0,
+                0.0,
+                -5778.9473684210525,
+                6100000.0,
+                0.0,
+                0.0,
+                1.0,
+            ],
+        },
+    },
+    "label": "sinergise_s2bm_level1_1-0-20201011_55HFA_2020-10-11",
+    "lineage": {},
+    "measurements": {
+        "blue": {"grid": "998", "path": "B02.jp2"},
+        "coastal_aerosol": {
+            "grid": "5778",
+            "path": "B01.jp2",
+        },
+        "green": {"grid": "998", "path": "B03.jp2"},
+        "nir_1": {"grid": "998", "path": "B08.jp2"},
+        "nir_2": {"path": "B8A.jp2"},
+        "red": {"grid": "998", "path": "B04.jp2"},
+        "red_edge_1": {"path": "B05.jp2"},
+        "red_edge_2": {"path": "B06.jp2"},
+        "red_edge_3": {"path": "B07.jp2"},
+        "swir_1_cirrus": {
+            "grid": "5778",
+            "path": "B10.jp2",
+        },
+        "swir_2": {"path": "B11.jp2"},
+        "swir_3": {"path": "B12.jp2"},
+        "water_vapour": {
+            "grid": "5778",
+            "path": "B09.jp2",
+        },
+    },
+    "properties": {
+        "datetime": datetime.datetime(2020, 10, 11, 0, 6, 49, 882566),
+        "eo:cloud_cover": 24.9912,
+        "eo:gsd": 10,
+        "eo:instrument": "MSI",
+        "eo:platform": "sentinel-2b",
+        "eo:constellation": "sentinel-2",
+        "eo:sun_azimuth": 46.3307328858312,
+        "eo:sun_elevation": 37.3713908882192,
+        "odc:dataset_version": "1.0.20201011",
+        "odc:file_format": "JPEG2000",
+        "odc:processing_datetime": datetime.datetime(2020, 10, 11, 1, 47, 4, 112949),
+        "odc:producer": "sinergise.com",
+        "odc:product_family": "level1",
+        "odc:region_code": "55HFA",
+        "sentinel:grid_square": "FA",
+        "sentinel:latitude_band": "H",
+        "sentinel:utm_zone": 55,
+        "sinergise_product_id": "73e1a409-595d-4fbf-8fe0-01e0ee26bf00",
+        "sentinel:product_name": "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446",
+        "sentinel:datastrip_id": "S2B_OPER_MSI_L1C_DS_EPAE_20201011T011446_S20201011T000244_N02.09",
+        "sentinel:datatake_start_datetime": datetime.datetime(2020, 10, 11, 1, 14, 46),
+        "sentinel:sentinel_tile_id": "S2B_OPER_MSI_L1C_TL_EPAE_20201011T011446_A018789_T55HFA_N02.09",
+    },
+    "accessories": {
+        "metadata:sinergise_product_info": {"path": "productInfo.json"},
+        "metadata:s2_tile": {"path": "metadata.xml"},
+    },
+}
+
+
+@pytest.fixture(params=["esa", "sinergise"])
+def dataset_input_output(request, tmp_path):
+    datasets = dict(
+        esa=(
+            ESA_INPUT_DATASET,
+            ESA_EXPECTED_METADATA,
+            "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.odc-metadata.yaml",
+        ),
+        sinergise=(
+            SINERGISE_INPUT_DATASET,
+            SINERGISE_EXPECTED_METADATA,
+            "S2B_MSIL1C_20201011T000249_N0209_R030_T55HFA_20201011T011446.odc-metadata.yaml",
+        ),
+    )
+    input_dataset_path, expected_metadata_doc, expected_metadata_name = datasets[
+        request.param
+    ]
+    unique_in_path = tmp_path / input_dataset_path.name
+
+    if input_dataset_path.is_dir():
+        shutil.copytree(input_dataset_path, unique_in_path)
+        # A folder input expects a metadata file inside.
+        expected_metadata_path = (
+            tmp_path / input_dataset_path.name / expected_metadata_name
+        )
+    else:
+        shutil.copy(input_dataset_path, unique_in_path)
+        # A file expects a sibling metadata path.
+        expected_metadata_path = tmp_path / expected_metadata_name
+
+    return (
+        unique_in_path,
+        expected_metadata_doc,
+        expected_metadata_path,
+    )
+
+
+def test_generate_expected_outputs(tmp_path, dataset_input_output):
+    """
+    Run prepare on our test input scenes, and check the created metadata matches expected.
+    """
+    (
+        input_dataset_path,
+        expected_metadata_doc,
+        expected_metadata_path,
+    ) = dataset_input_output
+    check_prepare_outputs(
+        invoke_script=sentinel_l1_prepare.main,
+        run_args=[
+            input_dataset_path,
+        ],
+        expected_doc=expected_metadata_doc,
+        expected_metadata_path=expected_metadata_path,
+    )
