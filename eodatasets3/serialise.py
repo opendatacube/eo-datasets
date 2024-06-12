e6332b20036f3f6ee8df01382d6cac9986a430ce
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2024-06-12 05:05:17 +0000

    replace RefResolver with referencing

--- eodatasets3/serialise.py
+++ eodatasets3/serialise.py
@@ -159,19 +159,24 @@ def _is_json_array(checker, instance) -> bool:
     By default, jsonschema only allows a json array to be a Python list.
     Let's allow it to be a tuple too.
     """
-    return isinstance(instance, (list, tuple))
+    return isinstance(instance, list | tuple)
 
 
-def _load_schema_validator(p: Path) -> jsonschema.Draft6Validator:
+def _load_schema_validator(p: Path) -> jsonschema.Draft7Validator:
     """
     Create a schema instance for the file.
 
     (Assumes they are trustworthy. Only local schemas!)
     """
+    import referencing
+
+    if not p.is_file():
+        raise ValueError(f"Can only load local schemas. Could not find file {p!s}")
+    if p.suffix.lower() not in (".yaml", ".yml"):
+        raise ValueError(f"Unexpected file type {p.suffix}. Expected yaml")
     with p.open() as f:
         schema = _yaml().load(f)
-    validator = jsonschema.validators.validator_for(schema)
-    validator.check_schema(schema)
+    jsonschema.Draft7Validator.check_schema(schema)
 
     # Allow schemas to reference other schemas relatively
     def doc_reference(path):
@@ -179,15 +184,20 @@ def _load_schema_validator(p: Path) -> jsonschema.Draft6Validator:
         if not path.exists():
             raise ValueError(f"Reference not found: {path}")
         referenced_schema = next(iter(read_documents(path)))[1]
-        return referenced_schema
+        return referencing.Resource(referenced_schema, referencing.jsonschema.DRAFT7)
 
-    ref_resolver = jsonschema.RefResolver.from_schema(
-        schema, handlers={"": doc_reference}
+    if p.parent:
+        registry = referencing.Registry(retrieve=doc_reference)  # type: ignore[call-arg, var-annotated]
+    else:
+        registry = referencing.Registry()
+
+    validator = jsonschema.validators.extend(
+        jsonschema.Draft7Validator,
+        type_checker=jsonschema.Draft7Validator.TYPE_CHECKER.redefine(
+            "array", _is_json_array
+        ),
     )
-    custom_validator = jsonschema.validators.extend(
-        validator, type_checker=validator.TYPE_CHECKER.redefine("array", _is_json_array)
-    )
-    return custom_validator(schema, resolver=ref_resolver)
+    return validator(schema, registry=registry)
 
 
 DATASET_SCHEMA = _load_schema_validator(Path(__file__).parent / "dataset.schema.yaml")
